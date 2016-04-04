f3112b5b6f08758918297a25725baac768026d4b
nekogamiyuki <NekoGamiYuki@users.noreply.github.com>
2016-04-04 00:26:13 +0000

    Fixed the amount of try blocks

--- users.py
+++ users.py
@@ -1,28 +1,36 @@
 class User(object):
+    """
+    This class contains simple information about the users within chat.
 
+    You can get their name, message, and what channel they chatted from.
+
+    usage:
+    newuser = User(response_from_twitch_server)
+    print(newuser.get_name())
+    print(newuser.get_message())
+    print(newuser.get_channel_from())
+    """
     def __init__(self,response):
+        # Why the try block? Because the first few messages from twitch
+        # cause some issues and I haven't found a better way to handle
+        # them yet. 
         try:
             self.info = response.split(':')
-            self.message = self.info[2]
+            self.message = self.info[2].rstrip()
             self.name = self.info[1].split("!")[0]
             self.channel_from = self.info[1].split('#')[1]
         except:
-            pass
-            
+            self.message = "MESSAGE_UNAVAILABLE"
+            self.name = "NAME_UNAVAILABLE"
+            self.channel_from = "CHANNEL_UNAVAILABLE"
+    
     def get_name(self):
-        try:
             return self.name
-        except:
-            return "NAME_UNAVAILABLE"
 
     def get_message(self):
-        try:
             return self.message.rstrip()
-        except:
-            return "MESSAGE_UNAVAILABLE"
+
 
     def get_channel_from(self):
-        try:
             return self.channel_from
-        except:
-            return "CHANNEL_UNAVAILABLE"
+
